b41fdc952e2828053ff2797b2f80cc91aa4ac991
Vincent222372 <vincentbrando0@gmail.com>
2026-03-03 14:58:58 +0700

    gay

---
+++ Models/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Models
+{
+    public class Roles
+    {
+        [Key] // Primary Key
+        public int RoleId { get; set; }
+
+        [Required] // Required field
+        [StringLength(50)]
+        public string RoleName { get; set; }
+
+
+
+    }
+}
